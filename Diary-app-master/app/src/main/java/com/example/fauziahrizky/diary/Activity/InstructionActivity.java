e621736845c9ac5bd898faee810fe3145e87833c
kikifauziaa <fauziahrizky22@gmail.com>
2019-04-13 08:42:37 +0700

    first commit

---
+++ Diary-app-master/app/src/main/java/com/example/fauziahrizky/diary/Activity/InstructionActivity.java
@@ -0,0 +1,19 @@
+package com.example.fauziahrizky.diary.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.fauziahrizky.diary.R;
+
+public class InstructionActivity extends AppCompatActivity {
+    //TextView instructionTv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instruction);
+
+       // instructionTv = findViewById(R.id.instructionTextView);
+
+
+    }
+}
